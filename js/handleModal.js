13aa6fb76c74ee6a78b5473fbe1736e29e37582d
AlexisGalliot <alexis@galliot.pro>
2016-12-30 17:13:07 +0100

    modal caserne

--- js/handleModal.js
+++ js/handleModal.js
@@ -1,3 +1,21 @@
 function handleModal(name) {
-  console.log(name);
+	console.log(name)
+	  if (name == 'caserne') {
+		  $('#modal_caserne').modal('open');
+	  }
 }
+
+function calc_nbr_moustique(evenement) {
+	var nbr = $('.nbr_moustique').val();
+	if (nbr >= 0) {
+		if (evenement == 1)
+			$('.nbr_moustique').val(parseFloat(nbr) + 1);
+		else if (evenement == 2)
+			$('.nbr_moustique').val(parseFloat(nbr) - 1);
+	}
+  else {
+	  $('.nbr_moustique').val(0);
+  }
+  Materialize.updateTextFields();
+}
+
--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,3 @@
-function calc_nbr_moustique(evenement) {
-	var nbr = $('.nbr_moustique').val();
-	if (nbr >= 0) {
-		if (evenement == 1)
-			$('.nbr_moustique').val(parseFloat(nbr) + 1);
-		else if (evenement == 2)
-			$('.nbr_moustique').val(parseFloat(nbr) - 1);
-	}
-  else {
-	  $('.nbr_moustique').val(0);
-  }
-  Materialize.updateTextFields();
-}
-
 (function() {
   $('.button_moins').click(function() {
 	  calc_nbr_moustique(2)
