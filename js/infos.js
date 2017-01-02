5f07e3d7c91dd7a7b34ec6b690c1f830fa2912fb
Sullivan Maxwell <sullivan.maxwell.pro@gmail.com>
2017-01-02 23:36:17 +0100

    add new circle progress design

---
+++ js/infos.js
@@ -0,0 +1,21 @@
+$(function(){
+    setTimeout(window.randomize, 200);
+    $('.ko-progress-circle').click(window.randomize);
+     $('.ko-progress-circle__slice span').html(percent+'%');
+});
+
+function setBloodA() {
+    return ;
+}
+function setBloodB() {
+    return ;
+}
+function setBloodAB() {
+    return ;
+}
+function setBlood0() {
+    return ;
+}
+function setLevel() {
+    return ;
+}
