00f66f50303ea5c7d6678335bf82fe4aa6dfde95
BOEHM Erwan <boehm_e@etna-alternance.net>
2017-01-06 09:03:40 +0100

    working + -, modal caserne

--- js/handleModal.js
+++ js/handleModal.js
@@ -1,54 +1,50 @@
-function calc_nbr_moustique(evenement) {
-    var nbr = $('.nbr_moustique').val();
-    if (nbr >= 0) {
-        if (evenement == 1)
-            $('.nbr_moustique').val(parseFloat(nbr) + 1);
-        else if (evenement == 2)
-            $('.nbr_moustique').val(parseFloat(nbr) - 1);
-    }
-    else {
-        $('.nbr_moustique').val(0);
-    }
-    Materialize.updateTextFields();
-}
 
 (function() {
-    $('.button_moins').click(function() {
-        calc_nbr_moustique(2)
-    });
+  $('#modal_caserne .moins').click(function(){
+    var nbr = parseInt($('#modal_caserne .nbr_moustique').val()) || 0;
+    var tmp = 0;
+    if (nbr > 1)
+      tmp = nbr - 1;
+    $('#modal_caserne .nbr_moustique').val(tmp);
+    Materialize.updateTextFields();
+  })
 
-    $('.button_plus').click(function() {
-        calc_nbr_moustique(1)
-    })
+  $('#modal_caserne .plus').click(function(){
+    var nbr = parseInt($('#modal_caserne .nbr_moustique').val()) || 0;
+    var tmp = nbr + 1;
+    $('#modal_caserne .nbr_moustique').val(tmp);
+    Materialize.updateTextFields();
+  })
 })();
 
 function handleModal(name) {
-	console.log(name)
-	  if (name == 'caserne') {
-		  $('#modal_caserne').modal('open');
-	  }
-	  else if (name == 'bloodhouse') {
-		  $('#modal_bloodhouse').modal('open');
-	  }
+  switch (name) {
+    case "caserne":
+    $('#modal_caserne').modal('open');
+    break;
+    case "caserne":
+    $('#modal_bloodhouse').modal('open');
+    break;
+  }
 }
 var slideContainer;
 (function() {
 
-    slideContainer = $('.slide-container');
+  slideContainer = $('.slide-container');
 
-    slideContainer.slick();
+  slideContainer.slick();
 
-    $('.clash-card__image img').hide();
+  $('.clash-card__image img').hide();
+  $('.slick-active').find('.clash-card img').fadeIn(200);
+
+  // On before slide change
+  slideContainer.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+    $('.slick-active').find('.clash-card img').fadeOut(1000);
+  });
+
+  // On after slide change
+  slideContainer.on('afterChange', function(event, slick, currentSlide) {
     $('.slick-active').find('.clash-card img').fadeIn(200);
-
-    // On before slide change
-    slideContainer.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-        $('.slick-active').find('.clash-card img').fadeOut(1000);
-    });
-
-    // On after slide change
-    slideContainer.on('afterChange', function(event, slick, currentSlide) {
-        $('.slick-active').find('.clash-card img').fadeIn(200);
-    });
+  });
 
 })();
